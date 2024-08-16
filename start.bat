959f0521734be6fd837304d5f71284477806a7bb
nighthawk <sucklyf@yahoo.com>
2024-08-16 05:51:46 +0300

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+title Nighthawk- C# Vanity Sniper - discord.gg/1966 -> @nightsocial
+color c
+cls
+
+:a
+dotnet run --project "C:\Kullanıcılar\SeninAdın\Projeler\index" "<- BURAYI DOSYA PATH İLE DEĞİŞ VE BU MESAJI SİL"
+goto a
+
+pause
